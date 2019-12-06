fa278b8764a5ddcc0c5a027c417212334901b45b
manny <yjm9554@gmail.com>
2019-12-07 00:28:41 +0900

    각 웨이브마다 남은 적 수만큼 체력에 데미지, 게임오버 구현

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -12,7 +12,7 @@ public class GameMaster : MonoBehaviour
 	public Transform Tier3TowerPrefab;
 	public Transform TowerSpawnPoint;
 
-	public float timeBetweenWaves = 50f;
+	public float timeBetweenWaves = 10f;
 
 	public Text waveCountdownText;
 	public Text hpText;
@@ -24,23 +24,50 @@ public class GameMaster : MonoBehaviour
 
 	private int waveNumber = 1;
 
-	private int[] numOfEnemies = new int[10] {50, 50, 50, 50, 50, 50, 50, 50, 50, 50};
+	private int[] numOfEnemies = new int[10] {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
 
 
 	void Start()
 	{
 		hp = 0;
 		money = 0;
-		HpChange(30);
+		HpChange(20);
 		MoneyChange(40);
+		GameObject.Find("GameoverUI").GetComponent<Canvas>().enabled = false;
 	}
 
 	void Update()
 	{
+		checkCountDown();
+	}
+
+	void checkCountDown()
+	{
 		if (countdown <= 0f)
 		{
-			StartCoroutine(SpawnWave());
-			countdown = timeBetweenWaves;
+			// 남아있는 enemy가 몇 개인지 세고, 모든 enemy를 없앤다.
+			GameObject[] remainedEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+			int remainedEnemiesCount = remainedEnemies.Length;
+			
+			for (int i=0;i<remainedEnemiesCount;i++){
+				Destroy(remainedEnemies[i]);
+			}
+
+			// 그만큼 체력을 깎는다.
+			HpChange(-remainedEnemiesCount);
+
+			// 체력이 0이하로 떨어지면 게임 오버가 되도록 한다.
+			if (hp <= 0 )
+			{
+				GameObject.Find("GameoverUI").GetComponent<Canvas>().enabled = true;
+				GameObject.Find("UI").GetComponent<Canvas>().enabled = false;
+			}
+			// 체력이 남아있다면 게임이 지속된다.
+			else 
+			{
+				StartCoroutine(SpawnWave());	
+				countdown = timeBetweenWaves;
+			}
 		}
 
 		countdown -= Time.deltaTime;
