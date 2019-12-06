6d9fd931efb2f85794a643934bb895f6091d8289
minseungkang <alstmd3@naver.com>
2019-12-07 01:36:57 +0900

    타워 리소스 xml 추가. Attribute enum 클래스 추가.

---
+++ Assets/Scripts/TowerAttributes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAttributes : MonoBehaviour
+{
+    public enum Attributes
+    {
+        UnitId, Name, TowerType, Atk, Rank, IsGOTY
+    }
+}
